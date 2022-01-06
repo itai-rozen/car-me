acc3711d73eab0e2c9b9a6c9c3752de2c0f3caa6
itai rozen <itai.rozen@mail.huji.ac.il>
2022-01-06 02:59:52 +0200

    changed app name :O also changed bits of description from homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -7,16 +7,16 @@ const Homepage = () => {
         <div className="center-container">
 
         <div className="about">
-            <h1>Car Me</h1>
+            <h1>CarKnowledge</h1>
             <div className="secondary-header-container">
             <h2>Helping you find your next car</h2> 
-                <h2>with minimum effort.</h2>
+                <h2>with a bit effort and a lot of knowledge. </h2>
             </div>
         </div>
         <aside className='aside'>
             <div className="description">
             Find complaints, recalls, safety ratings & reviews on your next car.
-                Start <Link to="/select"> browsing our database</Link>. <br /> <br />  you can also <Link to="/user">Sign in </Link> 
+                Start <Link to="/select"> browsing our database</Link> today! <br /> <br />  you can also <Link to="/user">Sign in </Link> 
                 and manage your favorite models. 
             </div>
         </aside>
